f9939ac621ef5f1c5c3edefe22996b5130ddbfa6
ozakiryota <kb22056@gmail.com>
2019-09-22 21:37:10 +0900

    updated

--- src/planar_feature_extraction.cpp
+++ src/planar_feature_extraction.cpp
@@ -5,39 +5,48 @@
 #include <pcl/point_types.h>
 #include <pcl/segmentation/conditional_euclidean_clustering.h>
 #include <pcl/common/common.h>
-#include <pcl/common/centroid.h>
+/* #include <pcl/common/centroid.h> */
 #include <pcl/filters/extract_indices.h>
 #include <pcl/visualization/cloud_viewer.h>
 
 #include "planar_landmark_ekf_slam/PlanarFeature.h"
 #include "planar_landmark_ekf_slam/PlanarFeatureArray.h"
 
-class ConditionalEuclideanClustering{
+class PlanarFeatureExtraction{
 	private:
 		/*node handle*/
 		ros::NodeHandle nh;
 		ros::NodeHandle nhPrivate;
 		/*subscribe*/
 		ros::Subscriber sub_nc;
-		/*pcl objects*/
+		/*publish*/
+		ros::Publisher pub_features;
+		/*objects*/
 		pcl::visualization::PCLVisualizer viewer {"Planar Feature Extraction"};
 		pcl::PointCloud<pcl::PointNormal>::Ptr normals {new pcl::PointCloud<pcl::PointNormal>};
 		std::vector<pcl::PointCloud<pcl::PointNormal>::Ptr> clusters;
+		pcl::PointCloud<pcl::PointNormal>::Ptr vis_features {new pcl::PointCloud<pcl::PointNormal>};
+		planar_landmark_ekf_slam::PlanarFeatureArray features;
 		/*parameters*/
 		double cluster_tolerance;
 		int min_cluster_size;
 	public:
-		ConditionalEuclideanClustering();
+		PlanarFeatureExtraction();
 		void CallbackNC(const sensor_msgs::PointCloud2ConstPtr &msg);
+		void ClearArray(void);
 		void Clustering(void);
 		static bool CustomCondition(const pcl::PointNormal& seedPoint, const pcl::PointNormal& candidatePoint, float squaredDistance);
+		Eigen::Vector4d ComputeAverageNormal(pcl::PointIndices indices);
 		void Visualization(void);
+		void Publication(void);
 };
 
-ConditionalEuclideanClustering::ConditionalEuclideanClustering()
+PlanarFeatureExtraction::PlanarFeatureExtraction()
 	:nhPrivate("~")
 {
-	sub_nc = nh.subscribe("/normals", 1, &ConditionalEuclideanClustering::CallbackNC, this);
+	sub_nc = nh.subscribe("/normals", 1, &PlanarFeatureExtraction::CallbackNC, this);
+	pub_features = nh.advertise<planar_landmark_ekf_slam::PlanarFeatureArray>("/features", 1);
+	
 	viewer.setBackgroundColor(1, 1, 1);
 	viewer.addCoordinateSystem(1.0, "axis");
 	viewer.setCameraPosition(0.0, 0.0, 35.0, 0.0, 0.0, 0.0);
@@ -48,20 +57,29 @@ ConditionalEuclideanClustering::ConditionalEuclideanClustering()
 	std::cout << "min_cluster_size = " << min_cluster_size << std::endl;
 }
 
-void ConditionalEuclideanClustering::CallbackNC(const sensor_msgs::PointCloud2ConstPtr &msg)
+void PlanarFeatureExtraction::CallbackNC(const sensor_msgs::PointCloud2ConstPtr &msg)
 {
 	/* std::cout << "CALLBACK PC" << std::endl; */
 
 	pcl::fromROSMsg(*msg, *normals);
 	std::cout << "==========" << std::endl;
 	std::cout << "normals->points.size() = " << normals->points.size() << std::endl;
+	features.header = msg->header;
 
-	clusters.clear();
+	ClearArray();
 	Clustering();
 	Visualization();
+	Publication();
 }
 
-void ConditionalEuclideanClustering::Clustering(void)
+void PlanarFeatureExtraction::ClearArray(void)
+{
+	features.features.clear();
+	clusters.clear();
+	vis_features->points.clear();
+}
+
+void PlanarFeatureExtraction::Clustering(void)
 {
 	double time_start = ros::Time::now().toSec();
 
@@ -87,14 +105,35 @@ void ConditionalEuclideanClustering::Clustering(void)
 		getMinMax3D(*normals, cluster_indices[i], Min, Max);
 		std::cout << i << ": Min  = " << std::endl << Min << std::endl;
 		std::cout << i << ": Max  = " << std::endl << Max << std::endl;
-		/*compute centroid*/
-		Eigen::Vector4f centroid;
-		pcl::compute3DCentroid(*normals, cluster_indices[i], centroid);
+		/*average normal*/
+		Eigen::Vector4d AveNormal = ComputeAverageNormal(cluster_indices[i]);
+		/* #<{(|compute centroid|)}># */
+		/* Eigen::Vector4f centroid; */
+		/* pcl::compute3DCentroid(*normals, cluster_indices[i], centroid); */
 		/*input*/
-
-		/*extract for visualization*/
+		planar_landmark_ekf_slam::PlanarFeature tmp_feature;
+		tmp_feature.x = -fabs(AveNormal(3))*AveNormal(0);
+		tmp_feature.y = -fabs(AveNormal(3))*AveNormal(1);
+		tmp_feature.z = -fabs(AveNormal(3))*AveNormal(2);
+		tmp_feature.min_x = Min[0];
+		tmp_feature.min_y = Min[1];
+		tmp_feature.min_z = Min[2];
+		tmp_feature.max_x = Max[0];
+		tmp_feature.max_y = Max[1];
+		tmp_feature.max_z = Max[2];
+		features.features.push_back(tmp_feature);
+		/*input for visualization*/
+		pcl::PointNormal tmp_normal;
+		tmp_normal.x = 0;
+		tmp_normal.y = 0;
+		tmp_normal.z = 0;
+		tmp_normal.data_n[0] = -fabs(AveNormal(3))*AveNormal(0);
+		tmp_normal.data_n[1] = -fabs(AveNormal(3))*AveNormal(1);
+		tmp_normal.data_n[2] = -fabs(AveNormal(3))*AveNormal(2);
+		vis_features->points.push_back(tmp_normal);
+		/*extraction for visualization*/
 		pcl::PointCloud<pcl::PointNormal>::Ptr tmp_cluster (new pcl::PointCloud<pcl::PointNormal>);
-		pcl::PointIndices::Ptr tmp_clustered_indices (new pcl::PointIndices);	//Can this be skipped?
+		pcl::PointIndices::Ptr tmp_clustered_indices (new pcl::PointIndices);
 		*tmp_clustered_indices = cluster_indices[i];
 		ei.setIndices(tmp_clustered_indices);
 		ei.filter(*tmp_cluster);
@@ -104,7 +143,7 @@ void ConditionalEuclideanClustering::Clustering(void)
 	std::cout << "clustering time [s] = " << ros::Time::now().toSec() - time_start << std::endl;
 }
 
-bool ConditionalEuclideanClustering::CustomCondition(const pcl::PointNormal& seedPoint, const pcl::PointNormal& candidatePoint, float squaredDistance)
+bool PlanarFeatureExtraction::CustomCondition(const pcl::PointNormal& seedPoint, const pcl::PointNormal& candidatePoint, float squaredDistance)
 {
 	Eigen::Vector3d N1(
 		seedPoint.normal_x,
@@ -123,7 +162,24 @@ bool ConditionalEuclideanClustering::CustomCondition(const pcl::PointNormal& see
 	else	return false;
 }
 
-void ConditionalEuclideanClustering::Visualization(void)
+Eigen::Vector4d PlanarFeatureExtraction::ComputeAverageNormal(pcl::PointIndices indices)
+{
+	Eigen::Vector4d Ave(0.0, 0.0, 0.0, 0.0);
+	for(size_t i=0;i<indices.indices.size();++i){
+		Eigen::Vector4d N(
+			normals->points[indices.indices[i]].data_n[0],
+			normals->points[indices.indices[i]].data_n[1],
+			normals->points[indices.indices[i]].data_n[2],
+			normals->points[indices.indices[i]].data_n[3]
+		);
+		Ave += N;
+	}
+	Ave /= (double)indices.indices.size();
+
+	return Ave;
+}
+
+void PlanarFeatureExtraction::Visualization(void)
 {
 	viewer.removeAllPointClouds();
 
@@ -152,15 +208,24 @@ void ConditionalEuclideanClustering::Visualization(void)
 		viewer.setPointCloudRenderingProperties (pcl::visualization::PCL_VISUALIZER_COLOR, rgb[0], rgb[1], rgb[2], name);
 		viewer.setPointCloudRenderingProperties (pcl::visualization::PCL_VISUALIZER_LINE_WIDTH, 1, name);
 	}
+	/*features*/
+	viewer.addPointCloudNormals<pcl::PointNormal>(vis_features, 1, 1.0, "vis_features");
+	viewer.setPointCloudRenderingProperties (pcl::visualization::PCL_VISUALIZER_COLOR, 0.0, 1.0, 1.0, "vis_features");
+	viewer.setPointCloudRenderingProperties (pcl::visualization::PCL_VISUALIZER_LINE_WIDTH, 2, "vis_features");
 
 	viewer.spinOnce();
 }
 
+void PlanarFeatureExtraction::Publication(void)
+{
+	pub_features.publish(features);
+}
+
 int main(int argc, char** argv)
 {
-    ros::init(argc, argv, "euclidean_clustering");
+    ros::init(argc, argv, "planar_feature_extraction");
 	
-	ConditionalEuclideanClustering euclidean_clustering;
+	PlanarFeatureExtraction planar_feature_extraction;
 
 	ros::spin();
 }
